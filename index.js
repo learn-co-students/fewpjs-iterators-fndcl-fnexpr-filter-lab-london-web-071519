c6fc9fe8117b3e88422c1ee33d0db2920d84db98
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-09-02 17:23:08 +0100

    filter method JS

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(array, string) {
+    return array.filter(driver => {
+        return driver.toUpperCase() === string.toUpperCase()
+    })
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(driver => {
+        return driver.toUpperCase().slice(0,2) === string.toUpperCase().slice(0,2)
+ })
+}
+
+function matchName(arrayOfObjects, string) {
+    return arrayOfObjects.filter(driver => {
+        return driver.name.toUpperCase() === string.toUpperCase()
+    })
+}
